c2702f08477ed2ae046043f92126fc6073395989
Chansin Thongkum <59070038@it.kmitl.ac.th>
2018-11-08 01:21:05 +0700

    set adapter on SlidingImageAdapter

--- app/src/main/java/com/review/foodreview/slideshow/SlidingImageAdapter.java
+++ app/src/main/java/com/review/foodreview/slideshow/SlidingImageAdapter.java
@@ -1,4 +1,63 @@
 package com.review.foodreview.slideshow;
+import android.content.Context;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
-public class SlidingImageAdapter {
+import com.review.foodreview.R;
+
+import java.util.ArrayList;
+
+public class SlidingImageAdapter extends PagerAdapter{
+    private ArrayList<ImageModel> imageModelArrayList;
+    private LayoutInflater inflater;
+    private Context context;
+
+    public SlidingImageAdapter(Context context,
+                               ArrayList<ImageModel> imageModelsArrayList){
+        this.context = context;
+        this.imageModelArrayList = imageModelsArrayList;
+        inflater = LayoutInflater.from(context);
+    }
+
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+    @Override
+    public int getCount() {
+        return imageModelArrayList.size();
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup view, int position) {
+        View imageLayout = inflater.inflate(R.layout.discover_slideshow, view, false);
+
+        assert imageLayout != null;
+        final ImageView imageView = (ImageView) imageLayout
+                .findViewById(R.id.image);
+
+
+        imageView.setImageResource(imageModelArrayList.get(position).getImage_drawable());
+
+        view.addView(imageLayout, 0);
+
+        return imageLayout;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view.equals(object);
+    }
+
+    @Override
+    public void restoreState(Parcelable state, ClassLoader loader) {
+    }
+
+    @Override
+    public Parcelable saveState() {
+        return null;
+    }
 }
