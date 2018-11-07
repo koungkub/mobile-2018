cd626a3a5feeb5c6fe12d563429ca297ad59b601
Chansin Thongkum <59070038@it.kmitl.ac.th>
2018-11-08 00:40:15 +0700

    Create init class

--- app/src/main/java/com/review/foodreview/MainActivity.java
+++ app/src/main/java/com/review/foodreview/MainActivity.java
@@ -8,6 +8,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.discover);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void init(Bundle bundle){
     }
 }
