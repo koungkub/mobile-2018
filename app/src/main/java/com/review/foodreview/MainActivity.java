fdd08955169aa0380063831e349bd90f9a458976
Chansin Thongkum <59070038@it.kmitl.ac.th>
2018-11-08 00:51:37 +0700

    modify init class and use in onCreate

--- app/src/main/java/com/review/foodreview/MainActivity.java
+++ app/src/main/java/com/review/foodreview/MainActivity.java
@@ -2,6 +2,7 @@ package com.review.foodreview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,9 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        init(savedInstanceState);
     }
     public void init(Bundle bundle){
-
+        if(bundle == null){
+            DiscoverFragment fragment = new DiscoverFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.main_view, fragment).commit();
+        }
     }
 }
