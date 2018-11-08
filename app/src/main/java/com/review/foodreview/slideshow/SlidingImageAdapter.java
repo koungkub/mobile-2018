61abfa82892fc3b62e8b64f1a972cf19254c1e88
Chansin Thongkum <59070038@it.kmitl.ac.th>
2018-11-08 20:22:57 +0700

    add gradle and change min sdk 15 to 16

--- app/src/main/java/com/review/foodreview/slideshow/SlidingImageAdapter.java
+++ app/src/main/java/com/review/foodreview/slideshow/SlidingImageAdapter.java
@@ -2,6 +2,7 @@ package com.review.foodreview.slideshow;
 import android.content.Context;
 import android.os.Parcelable;
 import android.support.v4.view.PagerAdapter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,22 +11,25 @@ import android.widget.ImageView;
 import com.review.foodreview.R;
 
 import java.util.ArrayList;
+public class SlidingImageAdapter extends PagerAdapter {
 
-public class SlidingImageAdapter extends PagerAdapter{
+
     private ArrayList<ImageModel> imageModelArrayList;
     private LayoutInflater inflater;
     private Context context;
 
-    public SlidingImageAdapter(Context context,
-                               ArrayList<ImageModel> imageModelsArrayList){
+
+    public SlidingImageAdapter(Context context, ArrayList<ImageModel> imageModelArrayList) {
         this.context = context;
-        this.imageModelArrayList = imageModelsArrayList;
+        this.imageModelArrayList = imageModelArrayList;
         inflater = LayoutInflater.from(context);
     }
 
+    @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
         container.removeView((View) object);
     }
+
     @Override
     public int getCount() {
         return imageModelArrayList.size();
@@ -33,15 +37,34 @@ public class SlidingImageAdapter extends PagerAdapter{
 
     @Override
     public Object instantiateItem(ViewGroup view, int position) {
-        View imageLayout = inflater.inflate(R.layout.discover_slideshow, view, false);
-
+        View imageLayout = inflater.inflate(R.layout.slidingimages_layout,
+                view,
+                false);
+        Log.d("ADAPTER", String.valueOf(position));
         assert imageLayout != null;
         final ImageView imageView = (ImageView) imageLayout
                 .findViewById(R.id.image);
-
-
-        imageView.setImageResource(imageModelArrayList.get(position).getImage_drawable());
-
+        if(position == 0){
+            Picasso.get()
+                    .load("https://www.knorr.com/content/dam/unilever/knorr_world/global/other_foods/all/regional_dishes-thai_north-eastern_dishes-hero_image-858794.jpg")
+                    .placeholder(R.drawable.hello)
+                    .into(imageView);
+        }
+        else if(position == 1){
+            Picasso.get()
+                    .load("https://www.knorr.com/content/dam/unilever/knorr_world/global/other_foods/all/type_of_dishes-international_dishes-hero_image-861954.jpg")
+                    .placeholder(R.drawable.hello2)
+                    .into(imageView);
+        }
+        else {
+            Picasso.get()
+                    .load("https://www.knorr.com/content/dam/unilever/knorr_world/global/other_foods/all/type_of_dishes-stir-fried-hero_image-862950.jpg")
+                    .placeholder(R.drawable.hello3)
+                    .into(imageView);
+        }
+//        imageView.setImageResource(imageModelArrayList
+//                .get(position)
+//                .getImage_drawable());
         view.addView(imageLayout, 0);
 
         return imageLayout;
@@ -60,4 +83,6 @@ public class SlidingImageAdapter extends PagerAdapter{
     public Parcelable saveState() {
         return null;
     }
+
+
 }
