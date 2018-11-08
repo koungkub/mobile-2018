a121210d8835fd10346a9b816a7848d8bb019aa7
DobaKung <zartre.y@gmail.com>
2018-11-08 18:14:25 +0700

    Add content to ListView in Discover

--- app/src/main/java/com/review/foodreview/DiscoverFragment.java
+++ app/src/main/java/com/review/foodreview/DiscoverFragment.java
@@ -7,8 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import com.review.foodreview.component.RestaurantListAdapter;
+
+import java.util.ArrayList;
 
 public class DiscoverFragment extends Fragment {
+    private ArrayList<Restaurant> restaurants = new ArrayList<>();
+
     @Nullable
     @Override
     public View onCreateView(
@@ -24,5 +30,31 @@ public class DiscoverFragment extends Fragment {
             @Nullable Bundle savedInstanceState
     ) {
         super.onActivityCreated(savedInstanceState);
+        restaurants.clear();
+
+        Restaurant mcdonalds = new Restaurant(
+                "McDonald's",
+                "Fast food",
+                "$ (< 120)",
+                "24 hours",
+                4.2f,
+                318,
+                true
+        );
+        Restaurant otoya = new Restaurant(
+                "Otoya",
+                "Japanese",
+                "$$ (120 - 300)",
+                "10.00 - 21.00",
+                4.6f,
+                282,
+                true
+        );
+        restaurants.add(mcdonalds);
+        restaurants.add(otoya);
+
+        final ListView _restaurantList = getView().findViewById(R.id.discover_list);
+        final RestaurantListAdapter restaurantAdapter = new RestaurantListAdapter(getActivity(),R.layout.restaurant_list_item, restaurants);
+        _restaurantList.setAdapter(restaurantAdapter);
     }
 }
