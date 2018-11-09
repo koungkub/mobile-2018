b1593fb08658f2c38aad61f473d90ee983281a40
Chansin Thongkum <59070038@it.kmitl.ac.th>
2018-11-09 20:11:32 +0700

    add navbar in search

--- app/src/main/java/com/review/foodreview/DiscoverFragment.java
+++ app/src/main/java/com/review/foodreview/DiscoverFragment.java
@@ -6,7 +6,6 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
