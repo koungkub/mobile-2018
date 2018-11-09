9a77ccc86f0d49fbd34adf5e42391b4247ffbb56
Chansin Thongkum <59070038@it.kmitl.ac.th>
2018-11-09 16:35:41 +0700

    Refactor

--- app/src/main/java/com/review/foodreview/SearchFragment.java
+++ app/src/main/java/com/review/foodreview/SearchFragment.java
@@ -4,14 +4,19 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class SearchFragment extends Fragment {
+    private static final String LOG = "SEARCHFRAGMENT";
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        Log.d(LOG, "Start SearchFragment");
         return inflater.inflate(R.layout.search, container, false);
     }
 
