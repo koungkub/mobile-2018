89d40e93fadf40fb4c08e6789f7b760d75217aa0
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-11-11 10:35:10 +0700

    Add toolbar title

--- app/src/main/java/com/review/foodreview/ReviewEditFragment.java
+++ app/src/main/java/com/review/foodreview/ReviewEditFragment.java
@@ -7,8 +7,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.v7.widget.Toolbar;
 
 public class ReviewEditFragment extends Fragment {
+    private Toolbar toolbar;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -18,5 +21,7 @@ public class ReviewEditFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        toolbar = getActivity().findViewById(R.id.review_edit_toolbar);
+        toolbar.setTitle("Write a review");
     }
 }
