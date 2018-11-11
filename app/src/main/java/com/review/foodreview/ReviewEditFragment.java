e029843fa6fbdb254840d2b80e086d126c013b60
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-11-11 10:19:10 +0700

    Add ReviewEdit fragment

---
+++ app/src/main/java/com/review/foodreview/ReviewEditFragment.java
@@ -0,0 +1,22 @@
+package com.review.foodreview;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ReviewEditFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.review_edit, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
