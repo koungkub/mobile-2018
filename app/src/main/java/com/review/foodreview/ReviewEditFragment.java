38e12f66ac4577bf6a10c69535ef93aed17a6c2f
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-11-11 11:17:16 +0700

    Add post and back icons to toolbar

--- app/src/main/java/com/review/foodreview/ReviewEditFragment.java
+++ app/src/main/java/com/review/foodreview/ReviewEditFragment.java
@@ -4,12 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.*;
+import android.widget.Toolbar;
 
 public class ReviewEditFragment extends Fragment {
+
+    private static final String TAG = "REVIEWEDIT";
     private Toolbar toolbar;
 
     @Nullable
@@ -21,7 +22,14 @@ public class ReviewEditFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        createMenu();
+    }
+
+    private void createMenu() {
         toolbar = getActivity().findViewById(R.id.review_edit_toolbar);
         toolbar.setTitle("Write a review");
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
+        toolbar.inflateMenu(R.menu.review_edit);
+        getActivity().setActionBar(toolbar);
     }
 }
