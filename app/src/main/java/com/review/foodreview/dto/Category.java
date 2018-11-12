dc0dfb50d076b2e8ac1aeaef0fcc77b4fced5584
Chansin Thongkum <59070038@it.kmitl.ac.th>
2018-11-12 20:05:38 +0700

    create User and Category model in dto

---
+++ app/src/main/java/com/review/foodreview/dto/Category.java
@@ -0,0 +1,10 @@
+package com.review.foodreview.dto;
+
+public class Category {
+    String id, name;
+
+    public Category(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/com/review/foodreview/dto/User.java
@@ -0,0 +1,10 @@
+package com.review.foodreview.dto;
+
+public class User {
+    String name, uuid;
+
+    public User(String name, String uuid) {
+        this.name = name;
+        this.uuid = uuid;
+    }
+}
