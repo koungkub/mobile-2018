a52e82017f4b76c60b89b6c670c430f83c4ad305
Chansin Thongkum <59070038@it.kmitl.ac.th>
2018-11-15 22:33:36 +0700

    fix bug navbar

--- app/src/main/java/com/review/foodreview/MeLoginFragment.java
+++ app/src/main/java/com/review/foodreview/MeLoginFragment.java
@@ -3,13 +3,16 @@ package com.review.foodreview;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -23,12 +26,8 @@ public class MeLoginFragment extends Fragment{
     private List<String> menu;
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
-    private ListView listMenu;
     public MeLoginFragment(){
         menu = new ArrayList<>();
-        menu.add("Bookmark");
-        menu.add("My review");
-        menu.add("Sign out");
 
     }
     @Nullable
@@ -57,24 +56,12 @@ public class MeLoginFragment extends Fragment{
 
     }
     private void showMenu(){
-        menuAdapter  = new ArrayAdapter<String>(
-                getActivity(),
-                android.R.layout.simple_list_item_1,
-                menu
-        );
-        listMenu = getView().findViewById(R.id.me_login_menulist);
-        listMenu.setAdapter(menuAdapter);
-        listMenu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        TextView bookmark = getActivity()
+                .findViewById(R.id.me_login_bookmark);
+        bookmark.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if(position == 2){
-                    mAuth.signOut();
-                    getActivity()
-                            .getSupportFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.main_view, new DiscoverFragment())
-                            .commit();
-                }
+            public void onClick(View v) {
+                mAuth.signOut();
             }
         });
     }
