91b770f5303e811c388f293ec95765f1c035ca96
Chansin Thongkum <59070038@it.kmitl.ac.th>
2018-11-15 23:31:20 +0700

    create bookmark

---
+++ app/src/main/java/com/review/foodreview/BookmarkFragment.java
@@ -0,0 +1,22 @@
+package com.review.foodreview;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BookmarkFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.bookmark, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
